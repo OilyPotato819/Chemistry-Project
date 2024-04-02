164ef5d2c0ef3ddd99b60f8123abe32a10fc5c24
David Stobbe <d.stobbe1@share.epsb.ca>
2024-04-02 17:40:37 -0600

    Co-authored-by: OilyPotato8190 <OilyPotato8190@users.noreply.github.com>

--- neural-net.py
+++ neural-net.py
@@ -9,25 +9,40 @@ from sklearn.metrics import r2_score
 import seaborn as sns
 
 data = pd.DataFrame(pd.read_csv("newdata.csv")).dropna()
-x = data.iloc[:, :6]
+x = data.iloc[:, [3, 4, 5]]
 y = data.iloc[:, 6]
 
 # matrix = data.corr()
 # plt.subplots(figsize=(25, 20))
 # sns.heatmap(matrix, cmap="Greens", annot=True)
 # plt.show()
-
 model = keras.Sequential()
-model.add(keras.layers.Dense(5))
+model.add(keras.layers.Dense(6))
+model.add(keras.layers.Dense(6))
+model.add(keras.layers.Dense(6))
+model.add(keras.layers.Dense(6))
 model.add(keras.layers.Dense(1))
 x = tf.constant(x)
 y = tf.constant(y)
-model.compile(optimizer='sgd', loss='mse', metrics=["mse"])
-model.fit(x, y, epochs=1)
+# model.compile(optimizer=keras.optimizers.SGD(
+#     learning_rate=0.001), loss='mae', metrics=["mae"])
+model.compile(optimizer='Adam', loss='mae', metrics=["mae"])
+model.fit(x, y, epochs=25)
+predicted = model.predict(x)
+experimental = y.numpy()
+average = np.add(predicted, experimental)
 
-# model = keras.Sequential()
-# model.add(keras.Input(shape=(593, 6)))
-# model.add(keras.layers.Dense(8))
-# model.compile(optimizer='sgd', loss='mse')
+error = []
+for i in range(len(predicted)):
+    error.append(abs(predicted[i][0] - experimental[i]))
+average = sum(error) / len(error)
 
-# model.fit(tf.constant(x), tf.constant(y), batch_size=32, epochs=10)
+# test = np.array([x[0]])
+# print(model.predict(test)[0][0])
+# print(np.array(y[0]))
+
+# electronegativity = list(zip(*x.numpy()))[5]
+# plt.scatter(electronegativity, y)
+# plt.show()
+
+print("Average: " + str(average))

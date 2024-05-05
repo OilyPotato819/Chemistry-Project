eb1fdfeef3ce587a124cff40f21abe1a535dc7f8
OilyPotato819 <134105915+OilyPotato819@users.noreply.github.com>
2024-05-05 02:15:05 -0600

    Electron and Bond class, bonding pairs

--- website/script.js
+++ website/script.js
@@ -10,6 +10,7 @@ class Atom {
   constructor(x, y, speed, symbol, color) {
     this.x = x;
     this.y = y;
+    //random initial velocity
     this.vx = Math.random() * speed * 2 - speed;
     this.vy = Math.random() * speed * 2 - speed;
     this.color = color;
@@ -17,14 +18,12 @@ class Atom {
     Object.assign(this, elementData.get(symbol));
     this.r = this.covalentRadius;
     this.bonds = [];
-    this.fullyBonded = false;
-    this.furthestBond = { dist: -Infinity };
 
     const bondNum = this.valency + this.lonePairs;
-    for (let n = 0; n < bondNum; n++) {
-      const angle = (n + 2) * ((2 * Math.PI) / bondNum);
-      const atom = n < this.lonePairs ? 2 : 1;
-      this.bonds.push({ angle: angle, angularVelocity: 0, atom: atom });
+    for (let i = 0; i < bondNum; i++) {
+      const angle = i * ((2 * Math.PI) / bondNum);
+      const charge = i < this.lonePairs ? 2 : 1;
+      this.bonds.push(new Electron(this, angle, charge, i));
     }
   }
 
@@ -37,13 +36,12 @@ class Atom {
 
     for (let i = 0; i < this.bonds.length - 1; i++) {
       for (let j = i + 1; j < this.bonds.length; j++) {
-        this.repulseBonds(this.bonds[i], this.bonds[j]);
+        this.repulseElectrons(this.bonds[i].parentElectron || this.bonds[i], this.bonds[j].parentElectron || this.bonds[j]);
       }
     }
 
     for (const bond of this.bonds) {
-      bond.angle += bond.angularVelocity * elapsedTime;
-      bond.angularVelocity *= friction;
+      bond.update();
     }
 
     if (this.x - this.r < container.pos[0]) {
@@ -70,52 +68,27 @@ class Atom {
     this.vy += (components.y / this.atomicMass) * elapsedTime;
   }
 
-  applyTorque(magnitude, angle, bond) {
-    const torque = this.covalentRadius * magnitude * Math.sin(angle);
-    const inertia = this.covalentRadius ** 2;
-    const angularAcceleration = torque / inertia;
+  repulseElectrons(electron1, electron2) {
+    const x = electron1.x - electron2.x;
+    const y = electron1.y - electron2.y;
 
-    bond.angularVelocity += angularAcceleration * elapsedTime;
-  }
-
-  repulseBonds(bond1, bond2) {
-    const x = Math.cos(bond1.angle) - Math.cos(bond2.angle);
-    const y = Math.sin(bond1.angle) - Math.sin(bond2.angle);
-
-    const dist = Math.sqrt((this.covalentRadius * x) ** 2 + (this.covalentRadius * y) ** 2);
+    const dist = Math.sqrt(x ** 2 + y ** 2);
     const forceAngle = Math.atan2(y, x);
-    const force = electrostaticForce(bond1.atom, bond2.atom, dist);
+    const force = electrostaticForce(electron1.charge, electron2.charge, dist);
 
-    const angle1 = forceAngle - bond1.angle;
-    const angle2 = forceAngle + Math.PI - bond2.angle;
+    const angle1 = forceAngle - electron1.angle;
+    const angle2 = forceAngle + Math.PI - electron2.angle;
 
-    this.applyTorque(force, angle1, bond1);
-    this.applyTorque(force, angle2, bond2);
+    electron1.applyTorque(force, angle1);
+    electron2.applyTorque(force, angle2);
   }
 
-  setFurthestBond() {
-    let furthestBond = { dist: -Infinity };
-
-    for (const bondedAtom of this.bonds) {
-      const dist = calcDist(this, bondedAtom);
-      if (dist > furthestBond.dist) {
-        furthestBond = { atom: bondedAtom, dist: dist };
-      }
-    }
-
-    this.furthestBond = furthestBond;
+  createBond(atom, parentElectron, bondedElectron) {
+    this.bonds[parentElectron.index] = new Bond(this, atom, parentElectron, bondedElectron);
   }
 
-  createBond(atom) {
-    this.bonds.push(atom);
-    this.setFurthestBond();
-    this.fullyBonded = this.bonds.length === this.valency;
-  }
-
-  breakBond(atom) {
-    this.bonds.splice(this.bonds.indexOf(atom), 1);
-    this.setFurthestBond();
-    this.fullyBonded = this.bonds.length === this.valency;
+  breakBond(bond) {
+    this.bonds[bond.parentElectron.index] = bond.parentElectron;
   }
 
   draw() {
@@ -125,14 +98,64 @@ class Atom {
     ctx.fill();
 
     for (const bond of this.bonds) {
-      const x = this.x * scale + this.covalentRadius * scale * Math.cos(bond.angle);
-      const y = this.y * scale + this.covalentRadius * scale * Math.sin(bond.angle);
-
-      ctx.fillStyle = bond.atom == 1 ? 'red' : 'black';
-      ctx.beginPath();
-      ctx.arc(x, y, 5, 0, Math.PI * 2);
-      ctx.fill();
+      bond.draw();
     }
+  }
+}
+
+class Electron {
+  constructor(parentAtom, angle, charge, index) {
+    this.parentAtom = parentAtom;
+    this.atomRadius = parentAtom.covalentRadius;
+    this.angle = angle;
+    this.calcPosition();
+    this.angularVelocity = 0;
+    this.charge = charge;
+    this.index = index;
+    this.color = this.charge === 1 ? 'red' : 'black';
+  }
+
+  calcPosition() {
+    this.x = this.parentAtom.x + this.atomRadius * Math.cos(this.angle);
+    this.y = this.parentAtom.y + this.atomRadius * Math.sin(this.angle);
+  }
+
+  applyTorque(magnitude, angle) {
+    const torque = this.atomRadius * magnitude * Math.sin(angle);
+    const inertia = this.atomRadius ** 2;
+    const angularAcceleration = torque / inertia;
+
+    this.angularVelocity += angularAcceleration * elapsedTime;
+  }
+
+  update() {
+    this.angle += this.angularVelocity * elapsedTime;
+    this.calcPosition();
+    this.angularVelocity *= friction;
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x * scale, this.y * scale, 5, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+class Bond {
+  constructor(parentAtom, bondedAtom, parentElectron, bondedElectron) {
+    this.parentAtom = parentAtom;
+    this.bondedAtom = bondedAtom;
+    this.parentElectron = parentElectron;
+    this.bondedElectron = bondedElectron;
+  }
+
+  update() {
+    this.parentElectron.update();
+  }
+
+  draw() {
+    this.parentElectron.draw();
   }
 }
 
@@ -199,13 +222,13 @@ class Container {
   }
 }
 
-const maxRepulsion = 10;
+const maxRepulsion = 50;
 const simulationSpeed = 0.01;
 const scale = 0.5;
 const cor = 0.5;
 const friction = 0.999;
 const vibFreq = 0.1;
-const coulomb = 1000;
+const coulomb = 10000;
 
 let lastTime;
 let elapsedTime;
@@ -220,7 +243,9 @@ let container = new Container([0, cnv.width, 0, cnv.height]);
 //   atoms.push(new Atom(Math.random() * (cnv.width / scale), Math.random() * (cnv.height / scale), 1, symbol, color));
 // }
 
-atoms.push(new Atom(500, 500, 0, 'Cl', 'blue'));
+atoms.push(new Atom(800, 500, 0, 'N', 'blue'));
+atoms.push(new Atom(800, 700, 0, 'C', 'black'));
+// atoms.push(new Atom(650, 700, 0, 'H', 'blue'));
 // atoms.push(new Atom(400, 400, 0, 'H', 'red'));
 // atoms.push(new Atom(500, 350, 0, 'H', 'red'));
 
@@ -326,52 +351,94 @@ function kineticEnergy(mass, velocity) {
   return (mass * velocity ** 2) / 2;
 }
 
-function calcForces(elapsedTime) {
+function calcForces() {
   for (let i = 0; i < atoms.length - 1; i++) {
     for (let j = i + 1; j < atoms.length; j++) {
       const atom1 = atoms[i];
       const atom2 = atoms[j];
 
       const angle = calcAngle(atom1, atom2);
-      let dist = calcDist(atom1, atom2);
+      let atomDist = calcDist(atom1, atom2);
 
-      if (dist < atom1.r + atom2.r) {
-        resolveCollision(atom1, atom2, angle, dist);
-        dist = atom1.r + atom2.r;
+      if (atomDist < atom1.r + atom2.r) {
+        resolveCollision(atom1, atom2, angle, atomDist);
+        atomDist = atom1.r + atom2.r;
       }
+      const { closestBond, bonded } = getClosestBond(atom1, atom2);
+      if (!closestBond) continue;
 
-      // replaces furthest bond with current atom, if current atom is closer than bonded atom.
-      if (atom1.fullyBonded && atom1.furthestBond.dist >= atom2.furthestBond.dist && dist < atom1.furthestBond.dist) {
-        atom1.breakBond(atom1.furthestBond.atom);
-      } else if (atom2.fullyBonded && atom2.furthestBond.dist > atom1.furthestBond.dist && dist < atom2.furthestBond.dist) {
-        atom2.breakBond(atom2.furthestBond.atom);
-      }
+      const electron1 = closestBond.bond1.parentElectron || closestBond.bond1;
+      const electron2 = closestBond.bond2.parentElectron || closestBond.bond2;
+      const bondAngle = calcAngle(electron1, electron2);
 
-      const { force, shouldBond } = morseForce(atom1, atom2, dist);
-      const bonded = atom1.bonds.includes(atom2);
+      const bondDist = closestBond.dist + atom1.covalentRadius + atom2.covalentRadius;
+      const { force, shouldBond } = morseForce(atom1, atom2, bondDist);
 
-      if ((atom1.fullyBonded || atom2.fullyBonded) && !bonded) continue;
+      atom1.applyForce(force, bondAngle);
+      atom2.applyForce(force, bondAngle + Math.PI);
 
       if (shouldBond && !bonded) {
-        atom1.createBond(atom2);
-        atom2.createBond(atom1);
+        atom1.createBond(atom2, closestBond.bond1, closestBond.bond2);
+        atom2.createBond(atom1, closestBond.bond2, closestBond.bond1);
       } else if (!shouldBond && bonded) {
-        atom1.breakBond(atom2);
-        atom2.breakBond(atom1);
+        atom1.breakBond(closestBond.bond1);
+        atom2.breakBond(closestBond.bond2);
       }
-
-      atom1.applyForce(force, angle, elapsedTime);
-      atom2.applyForce(force, angle + Math.PI, elapsedTime);
     }
   }
 }
 
-function simulationStep(elapsedTime) {
+function getBondPairs(atom1, atom2) {
+  let bondPairs = [];
+  for (let bond1 of atom1.bonds) {
+    if (bond1 instanceof Electron && bond1.charge === 2) continue;
+    for (let bond2 of atom2.bonds) {
+      if (bond2 instanceof Electron && bond2.charge === 2) continue;
+
+      const bondDist = calcDist(bond1.parentElectron || bond1, bond2.parentElectron || bond2);
+      bondPairs.push({ bond1: bond1, bond2: bond2, dist: bondDist });
+    }
+  }
+
+  bondPairs.sort((a, b) => a.dist - b.dist);
+  return bondPairs;
+}
+
+function getClosestBond(atom1, atom2) {
+  const bondPairs = getBondPairs(atom1, atom2);
+
+  for (const bondPair of bondPairs) {
+    if (bondPair.bond1.bondedAtom === atom2) {
+      return { closestBond: bondPair, bonded: true };
+    }
+
+    const dist1 = bondPair.bond1 instanceof Bond ? calcDist(bondPair.bond1.parentElectron, bondPair.bond1.bondedElectron) : Infinity;
+    const dist2 = bondPair.bond2 instanceof Bond ? calcDist(bondPair.bond2.parentElectron, bondPair.bond2.bondedElectron) : Infinity;
+    const shouldBreakBonds = bondPair.dist < dist1 && bondPair.dist < dist2;
+
+    if (shouldBreakBonds && bondPair.bond1 instanceof Bond) {
+      atom1.breakBond(bondPair.bond1);
+      bondPair.bond1 = bondPair.bond1.parentElectron;
+    }
+    if (shouldBreakBonds && bondPair.bond2 instanceof Bond) {
+      atom2.breakBond(bondPair.bond2);
+      bondPair.bond2 = bondPair.bond2.parentElectron;
+    }
+
+    if (bondPair.bond1 instanceof Electron && bondPair.bond2 instanceof Electron) {
+      return { closestBond: bondPair, bonded: false };
+    }
+  }
+
+  return { closestBond: null, bonded: null };
+}
+
+function simulationStep() {
   container.update();
 
-  calcForces(elapsedTime);
+  calcForces();
   for (const atom of atoms) {
-    atom.update(elapsedTime);
+    atom.update();
   }
 }
 
